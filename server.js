823294373710e0f8eff3578123eec00003a8eae4
hakobuneshipping1 <business@hakobuneshipping.com>
2026-03-07 21:40:36 -0800

    Set up Express server with webhook endpoint

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+
+app.post("/webhook", (req, res) => {
+ console.log("Shopify order:", req.body);
+ res.send("Webhook received");
+});
+
+app.listen(3000, () => {
+ console.log("Server running");
+});
